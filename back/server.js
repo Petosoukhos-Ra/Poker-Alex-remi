9aa0e9a362f3e85ba106db9147fb955d67ff835a
R3m1r0 <remicanta65@gmail.com>
2022-11-30 14:34:52 +0100

    Commit multijoueur

--- back/server.js
+++ back/server.js
@@ -1,15 +1,19 @@
 import { Server } from "socket.io";
-import { createServer } from "http";
-import { onConnect } from "./serverActions.js";
-import { createGame } from "./gameActions.js";
 
-const httpServer = createServer();
-const io = new Server(httpServer, {
-  cors: {
-    origin: "*"
-  }
+const io = new Server({ /* options */ });
+
+const players = []
+
+io.on("connection", (socket) => {
+  console.log('User connected: ' + socket.id);
+  socket.emit('currentPlayers',players);
+  socket.broadcast.emit('newPlayer',players[socket.id]);
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected: ' + socket.id);
+    delete players[socket.id];
+    io.emit('disconnect', socket.id);
+  })
 });
-
-io.on("connection", onConnect )
 
 io.listen(3000);
